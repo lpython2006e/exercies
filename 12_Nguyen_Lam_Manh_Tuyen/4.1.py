042f08be1dae99e04fdb6c8f20dee8deb217c69c
manhtuyen2 <nguyenlammanhtuyen@gmail.com>
2020-07-20 00:31:12 +0700

    17072020

---
+++ 12_Nguyen_Lam_Manh_Tuyen/4.1.py
@@ -0,0 +1,11 @@
+#Create Student class with attributes like (Name, Nickname, birthday, class
+
+class student:
+    def __init__(self,name,nickname,birthday,classname):
+        self.name=name
+        self.nickname=nickname
+        self.birthday=birthday
+        self.classname=classname
+
+student1=student("Tuyen","TuyenNguyen","1412","python")
+print(student1.name,student1.nickname,student1.birthday,student1.classname)
---
+++ 12_Nguyen_Lam_Manh_Tuyen/4.2.py
@@ -0,0 +1,8 @@
+#Create a program to enter student information, store it as list of student and allow to search students by their name (partial)
+
+class student:
+    def __init__(self,name,nickname,birthday,classname):
+        self.name=input()
+        self.nickname=nickname
+        self.birthday=birthday
+        self.classname=classname
