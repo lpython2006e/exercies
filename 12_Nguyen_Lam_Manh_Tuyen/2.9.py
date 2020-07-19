09907677942ffda9b492d95c67548820f3a0e0a4
manhtuyen2 <nguyenlammanhtuyen@gmail.com>
2020-07-19 19:03:59 +0700

    17072020

--- 12_Nguyen_Lam_Manh_Tuyen/2.9.py
+++ 12_Nguyen_Lam_Manh_Tuyen/2.9.py
@@ -1,8 +1,8 @@
-#Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]
+# Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]
 def concat(list1, list2):
     lists = []
     for i in range(0, len(list1)):
-            lists.append(list1[i])
+        lists.append(list1[i])
     for i in range(0, len(list2)):
-            lists.append(list2[i])
+        lists.append(list2[i])
     return lists
