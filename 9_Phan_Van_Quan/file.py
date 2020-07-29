57a02a13a106a79acd04f56a35e7b762d0a85f77
phanvanquan <quanchinh1234@gmail.com>
2020-07-29 13:42:15 +0700

    baitap

---
+++ 9_Phan_Van_Quan/file.py
@@ -0,0 +1,2 @@
+Live Demo#!/usr/bin/python# Open a filefo = open("foo.txt", "wb")print "Name of the file: ", fo.name
+# Here it does nothing, but you can call it with read operation.fo.flush()# Close opend filefo.close()
